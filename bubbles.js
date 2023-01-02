53591e70d21f58fc83246a163c72e321ceb16f18
EdwardOakley <47759261+fatchild@users.noreply.github.com>
2023-01-02 00:32:58 +0000

    clean up

--- bubbles.js
+++ bubbles.js
@@ -33,30 +33,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const background = document.querySelector('.background');
-
-//     background.addEventListener('click', event => {
-//         const bubble = document.createElement("div");
-//         bubble.className = 'bubble';
-//         bubble.style.top = event.clientY+"px";
-//         bubble.style.left = event.clientX+"px";
-//         bubble.style.backgroundColor = "#"+Math.floor(Math.random()*16777215).toString(16);
-//         bubble.style.transitionDuration = Math.floor((Math.random() * 120)+1)+"s";
-//         bubble.style.transform = "skew("+Math.floor((Math.random() * 10)-5)+"deg)";
-    
-//         setTimeout(function () {
-//             bubble.classList.add("bubble-grow");
-//             let bubbleSize = Math.floor((Math.random() * 10000)+10);
-//             bubble.style.height = bubbleSize+"px";
-//             bubble.style.width = bubbleSize+"px";
-//             bubble.style.marginTop = "-"+(bubbleSize/2)+"px";
-//             bubble.style.marginLeft = "-"+(bubbleSize/2)+"px";
-//             bubble.style.borderRadius = Math.floor((Math.random() * bubbleSize))+"px";
-//         }, 1000);
-
-//         background.appendChild(bubble);
-//     });
-
-// });
